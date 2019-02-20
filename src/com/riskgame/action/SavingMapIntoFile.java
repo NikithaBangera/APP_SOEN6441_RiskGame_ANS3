995ee6c7b37d4e7b4631c04e44d302aacf426923
Shiva Shamloo Admin <s_hamlo_admin@macbean.encs.concordia.ca>
2019-02-20 11:34:49 -0500

    changed related to the path

--- src/com/riskgame/action/SavingMapIntoFile.java
+++ src/com/riskgame/action/SavingMapIntoFile.java
@@ -2,12 +2,14 @@ package com.riskgame.action;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 public class SavingMapIntoFile {
 
 	public void saveMapTag(String[] mapTagData) {
-		String filepath= "/Users/s_hamlo_admin/Desktop/APP_SOEN6441_RiskGame_ANS3-SumeethaShiva/maps/"+mapTagData[2];
-		
+		String workingDir = System.getProperty("user.dir");
+		String filepath= workingDir+"/maps/"+mapTagData[2];
+		//String fileName=mapTagData[2];
 		try {
 		File file = new File(filepath);
 		file.getParentFile().mkdirs(); 
