51d4b1280a0efe98b572a45946681913841754b7
Shiva Shamloo Admin <s_hamlo_admin@macbean.encs.concordia.ca>
2019-02-20 10:36:02 -0500

    saving the map files

--- src/com/riskgame/action/SavingMapIntoFile.java
+++ src/com/riskgame/action/SavingMapIntoFile.java
@@ -1,18 +1,26 @@
 package com.riskgame.action;
-
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
-
+import java.io.File;
+import java.io.IOException;
 public class SavingMapIntoFile {
 
 	public void saveMapTag(String[] mapTagData) {
-		String fileName=mapTagData[2];
+		String filepath= "/Users/s_hamlo_admin/Desktop/APP_SOEN6441_RiskGame_ANS3-SumeethaShiva/maps/"+mapTagData[2];
+		
 		try {
-		PrintWriter outputStream = new PrintWriter(fileName);
+		File file = new File(filepath);
+		file.getParentFile().mkdirs(); 
+		file.createNewFile();
+		PrintWriter outputStream = new PrintWriter(file);
 		outputStream.println(mapTagData[0]);
 		outputStream.println(mapTagData[1]);
+		outputStream.flush();
 		outputStream.close();
 		}catch(FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		catch(IOException e) {
 			e.printStackTrace();
 		}
 	}
