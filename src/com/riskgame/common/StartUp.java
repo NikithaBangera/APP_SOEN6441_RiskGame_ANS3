5b2626372873d3fd0580bcd2b6a5a3b6891312d7
Anusha KT <kt.anusha21@gmail.com>
2019-02-25 19:13:37 -0500

    Startup Phase

---
+++ src/com/riskgame/common/StartUp.java
@@ -0,0 +1,27 @@
+package com.riskgame.common;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.risk.services.MapIO;
+
+public class StartUp {
+	
+	private Map playersList = new HashMap();
+	private MapTag mapTag;
+	private int countOfthePlayers=0;
+	public minimumPlayerCount =2;
+	public maximumPlayercount=6;
+	
+	
+	public StartUp(MapTag mapTag) {
+	
+		
+		
+		
+		
+		
+		
+	}
+
+}
