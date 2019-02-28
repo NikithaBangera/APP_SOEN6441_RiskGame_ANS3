f9036d3a47b563e610a4bdc1537f0867189d7124
Shresthi Garg <shresthigarg04@gmail.com>
2019-02-28 13:36:50 -0500

    Changes related to fortification phase

--- src/com/riskgame/gameplay/FortificationPhase.java
+++ src/com/riskgame/gameplay/FortificationPhase.java
@@ -10,79 +10,66 @@ import com.riskgame.common.RiskPlayer;
 
 public class FortificationPhase {
 
-	public void startGameFortification() {
+	public void startGameFortification(RiskPlayer player) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		RiskPlayer player = new RiskPlayer();
-		GameMapGraph mapGraph = new GameMapGraph();
-		System.out.println("Fortification Phase starts!\n");
-		System.out.println("Player: " + player.getName() + "\n");
-		System.out.println("Do you want to continue with Fortification phase? (Yes or No)");
-		try {
-			if (br.readLine().trim().equalsIgnoreCase("Yes")) {
-				if (player.getMyCountries().size() >= 2) {
-					boolean flag = true;
-					String fromCountry = "";
-					String toCountry = "";
-					System.out.println("List of countries that player has are: ");
-					for (Country country : player.getMyCountries()) {
-						System.out.println("* " + country.getName() + ":" + country.getNoOfArmies() + "\n");
-					}
-					while (flag) {
-						flag = true;
-						System.out.println("Enter the name of country from which you want to move some armies :");
-						fromCountry = br.readLine();
-						System.out.println(
-								"Enter the name of country to which you want to move some armies, from country "
-										+ fromCountry);
-						toCountry = br.readLine();
-						if (!mapGraph.getCountrySet().containsKey(fromCountry.trim())
-								|| !mapGraph.getCountrySet().containsKey(toCountry.trim())) {
-							flag = false;
-							System.out.println("Please enter correct country name.");
-						}
-						Country givingCountry = mapGraph.getCountrySet().get(fromCountry.trim());
-						Country receviningCountry = mapGraph.getCountrySet().get(fromCountry.trim());
-						if (player.getMyCountries().contains(givingCountry)
-								&& player.getMyCountries().contains(receviningCountry)) {
-							flag = true;
-						} else {
-							System.out.println(
-									"Entered countries doesn't exist in player's owned countries, please enter country names again");
-							flag = false;
-						}
-					}
-					int countOfArmies = 0;
-					while (flag) {
-						flag = true;
-						System.out
-								.println("Enter the number of armies to move from " + fromCountry + " to " + toCountry);
-						try {
-							countOfArmies = Integer.parseInt(br.readLine());
-							if (countOfArmies > mapGraph.getCountrySet().get(fromCountry).getNoOfArmies()) {
-								System.out.println("Sufficient number of armies is not available.");
-								flag = false;
-							}
-
-						} catch (NumberFormatException e) {
-							System.out.println("Invalid number of armies.");
-						}
-					}
-
-					moveArmies(mapGraph.getCountrySet().get(fromCountry), mapGraph.getCountrySet().get(toCountry),
-							countOfArmies);
+		GameMapGraph mapData = new GameMapGraph();
+		int countOfArmies = 0;
+		while (!(player.getMyCountries().size() <= 2)) {
+			boolean doFortification = true;
+			String fromCountry = "";
+			String toCountry = "";
+			System.out.println("List of countries that player has are: ");
+			for (Country country : player.getMyCountries()) {
+				System.out.println("* " + country.getName() + ":" + country.getNoOfArmies() + "\n");
+			}
+			while (doFortification) {
+				doFortification = true;
+				System.out.println("Enter the name of country from which you want to move some armies :");
+				fromCountry = br.readLine();
+				System.out.println(
+						"Enter the name of country to which you want to move some armies, from country " + fromCountry);
+				toCountry = br.readLine();
+				if (!mapData.getCountrySet().containsKey(fromCountry.trim())
+						|| !mapData.getCountrySet().containsKey(toCountry.trim())) {
+					doFortification = false;
+					System.out.println("Country doesn't exist!");
+				}
+				Country givingCountry = mapData.getCountrySet().get(fromCountry.trim());
+				Country receivingCountry = mapData.getCountrySet().get(fromCountry.trim());
+				if (player.getMyCountries().contains(givingCountry)
+						&& player.getMyCountries().contains(receivingCountry)) {
+					doFortification = true;
 				} else {
-					System.out.println("Insufficient armies left.");
+					System.out.println(
+							"Entered countries doesn't exist in player's owned country list, please enter country names again");
+					doFortification = false;
+				}
+				System.out.println("Enter the number of armies to move from " + fromCountry + " to " + toCountry);
+				try {
+					countOfArmies = Integer.parseInt(br.readLine());
+					if (countOfArmies > mapData.getCountrySet().get(fromCountry).getNoOfArmies()) {
+						System.out.println(
+								"Insufficient armies available, fortification is not possible with asked number of armies.");
+						doFortification = false;
+					}
+
+				} catch (NumberFormatException e) {
+					System.out.println("Invalid number of armies.");
 				}
 			}
-		} catch (IOException e) {
-			e.printStackTrace();
+
+			moveArmies(mapData.getCountrySet().get(fromCountry), mapData.getCountrySet().get(toCountry), countOfArmies);
 		}
 	}
 
 	public void moveArmies(Country fromCountry, Country toCountry, int armiesCount) {
 		if (fromCountry.getAdjacentCountries().contains(toCountry)) {
-			fromCountry.setNoOfArmies(fromCountry.getNoOfArmies() - armiesCount);
-			toCountry.setNoOfArmies(toCountry.getNoOfArmies() + armiesCount);
+			int fromCountryArmy = fromCountry.getNoOfArmies();
+			int toCountryArmy = toCountry.getNoOfArmies();
+			fromCountry.setNoOfArmies(fromCountryArmy - armiesCount);
+			toCountry.setNoOfArmies(toCountryArmy + armiesCount);
+		} else {
+			System.out.println("Countries are not adjacanet!");
 		}
 	}
 
--- src/com/riskgame/gameplay/StartupPhase.java
+++ src/com/riskgame/gameplay/StartupPhase.java
@@ -1,33 +1,45 @@
 package com.riskgame.gameplay;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 import com.riskgame.common.Continent;
 import com.riskgame.common.RiskPlayer;
 
 public class StartupPhase {
-ArrayList<RiskPlayer> playersList;
-	
+	ArrayList<RiskPlayer> playersList;
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
 	public StartupPhase() {
 		this.playersList = new ArrayList<RiskPlayer>();
 	}
-	
-	public void gamePlay() {
+
+	public void gamePlay() throws IOException {
 		RiskPlayer player = new RiskPlayer();
-		int round  = 1;
+		int round = 1;
 		RoundRobinScheduler roundRobin = new RoundRobinScheduler(getPlayersList());
-		
-		while(round <= getPlayersList().size()) {
-			
+
+		while (round <= getPlayersList().size()) {
+
 			player = roundRobin.nextTurn();
-			
+
+			System.out.println("Fortification Phase starts!\n");
+			System.out.println("Player: " + player.getName() + "\n");
+			System.out.println("Do you wish to start the Fortification phase? (Yes or No)");
+			if (br.readLine().trim().equalsIgnoreCase("Yes")) {
+				FortificationPhase fortification = new FortificationPhase();
+				fortification.startGameFortification(player);
+			} else {
+				System.out.println("Exited the fortification stage!");
+			}
+
 		}
-		
+
 		ReinforcementPhase reinforcement = new ReinforcementPhase();
 		reinforcement.startReinforcement(player);
-		
-		FortificationPhase fortification = new FortificationPhase();
-		fortification.startGameFortification();
+
 	}
 
 	public ArrayList<RiskPlayer> getPlayersList() {
