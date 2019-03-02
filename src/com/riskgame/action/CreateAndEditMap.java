bbd7c4637dfcec018d99afd7844dfe14695dcc75
Shresthi Garg <shresthigarg04@gmail.com>
2019-03-02 15:55:15 -0500

    Fix for user entering option as blank and bug of removing adjacency

--- src/com/riskgame/action/CreateAndEditMap.java
+++ src/com/riskgame/action/CreateAndEditMap.java
@@ -40,6 +40,7 @@ public class CreateAndEditMap {
 
 	public boolean newMapCreation() throws Exception {
 		boolean exit = false;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		while (!exit) {
 			System.out.println("\nWelcome to Risk Game!");
 			System.out.println("\nChoose the below options to create a new map\n");
@@ -51,12 +52,14 @@ public class CreateAndEditMap {
 			System.out.println("10. Exit without Saving the map");
 
 			System.out.println("\nPlease enter your choice below:");
-			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-			int option = 0;
+			String option = br.readLine().trim();
+			while (option.isEmpty()) {
+				System.err.println("\nChoice cannot be blank. Please enter your choice below:");
+				System.out.flush();
+				option = br.readLine().trim();
+			}
 
-			// try {
-			option = Integer.parseInt(br.readLine());
-			switch (option) {
+			switch (Integer.parseInt(option)) {
 			case 1:
 				createMapTag();
 				break;
@@ -626,13 +629,13 @@ public class CreateAndEditMap {
 				ArrayList<String> adjacent1 = checklist.get(0).getAdjacentCountries();
 				ArrayList<String> adjacent2 = checklist.get(1).getAdjacentCountries();
 				if (adjacent1.contains(checklist.get(1).getName()) && adjacent2.contains(checklist.get(0).getName())) {
-					if (!adjacent1.contains(checklist.get(1).getName())) {
+					if (adjacent1.contains(checklist.get(1).getName())) {
 						adjacent1.remove(checklist.get(1).getName());
 						checklist.get(0).setAdjacentCountries(adjacent1);
 //						countrylist.add(checklist.get(0));
 
 					}
-					if (!adjacent2.contains(checklist.get(0).getName())) {
+					if (adjacent2.contains(checklist.get(0).getName())) {
 						adjacent2.remove(checklist.get(0).getName());
 						checklist.get(1).setAdjacentCountries(adjacent2);
 //						countrylist.add(checklist.get(1));
