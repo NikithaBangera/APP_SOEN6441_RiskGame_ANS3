607c18ac9e990b50b5fe743d6e3e5bfccc7e7180
Anusha KT <kt.anusha21@gmail.com>
2019-03-02 17:00:45 -0500

    Following Coding standards

---
+++ src/com/riskgame/action/test/CreateAndEditMapTest.java
@@ -0,0 +1,14 @@
+package com.riskgame.action.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CreateAndEditMapTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
