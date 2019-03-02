fa8e9aa20bf989dc7b4a2b5a02faf509b6d82566
Shiva Shamloo Admin <s_hamlo_admin@macbean.encs.concordia.ca>
2019-03-02 18:02:57 -0500

    Changes regarding editing the map till adding a country

--- src/com/riskgame/action/LoadAndEditMap.java
+++ src/com/riskgame/action/LoadAndEditMap.java
@@ -1,10 +1,13 @@
 package com.riskgame.action;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
@@ -59,17 +62,24 @@ public class LoadAndEditMap {
 			if (tagData_match.matches()) {
 				str = br_file.readLine();
 				str = str.replaceAll("\\[", "").replaceAll("\\]", "");
+//				HashMap<String,String> data=new HashMap<String,String>();
+//				data=mapGraph.getTagData();
 				while ((!continent_match.matches()) && (!str.trim().isEmpty())) {
+//					String[] metadata=str.split("=");
 					// finds the image name
+//					data.put(metadata[0], metadata[1]);
 					image_match = image_pattern.matcher(str.trim());
 					if (image_match.matches()) {
 						image_name = str.substring(6);
+						
 					}
 					System.out.println(str + "\n");
 					str = br_file.readLine();
 					str = str.replaceAll("\\[", "").replaceAll("\\]", "");
 					continent_match = continents_pattern.matcher(str.trim());
 				}
+//				mapGraph.setTagData(data);
+				
 			}
 			while (str.trim().isEmpty()) {
 				str = br_file.readLine();
@@ -116,7 +126,7 @@ public class LoadAndEditMap {
 				
 				while ((str = br_file.readLine()) != null) {
 					//HashMap<Country,ArrayList<Country>> connectivity= new HashMap<Country,ArrayList<Country>>();
-					
+					Continent continent=new Continent();
 					String[] ter = str.split(",");
 					// checking the x-values and y-values and creates a country object
 					if ((Integer.parseInt(ter[2]) < 400) && (Integer.parseInt(ter[1]) < 400)) {
@@ -131,6 +141,8 @@ public class LoadAndEditMap {
 						country.setxValue(ter[1]);
 						country.setyValue(ter[2]);
 						country.setContinent(ter[3]);
+						continent.getCountriesInContinent().add(country);
+						mapGraph.getContinents().put(ter[3],continent);
 						ArrayList<Country> adjacent = new ArrayList<Country>();
 						HashMap<String, Country> country_name= new HashMap<String, Country>();
 						//help_country
@@ -255,7 +267,7 @@ public class LoadAndEditMap {
 				visited=new HashMap<Country,Integer>();
 				return loadMap();
 			}
-			editMap(filepath);
+			editMap(fileName);
 			String image = mapImage(image_name);
 			return image;
 
@@ -272,7 +284,7 @@ public class LoadAndEditMap {
 		String mapedition = mapedit.trim();
 		while (mapedition != null) {
 			if (mapedition.equals("y")) {
-				edition();
+				edition(file);
 				editMap(file);
 				mapedition = "n";
 			} else if (mapedition.equals("n")) {
@@ -310,9 +322,9 @@ public class LoadAndEditMap {
 		return image;
 	}
 
-	public void edition() throws IOException {
+	public void edition(String file) throws IOException {
 		System.out.println("\nChoose the below options to edit the map:");
-		System.out.println("1. edit Map name or Author name:");
+		System.out.println("1. Edit map tag data:");
 		System.out.println("2. Add continents:\n3. Delete a continent:");
 		System.out.println("4. Add a country:\n5. Delete a country:");
 		System.out.println("6. Add adjacency:\n7. Delete Adjacency:");
@@ -323,30 +335,22 @@ public class LoadAndEditMap {
 			option = Integer.parseInt(br.readLine());
 		} catch (NumberFormatException e) {
 			System.out.println("\nPlease enter valid number");
-			edition();
+			edition(file);
 			return;
 		}
 		switch (option) {
 		case 1:
-			changeMetadata();
+			changeMetadata(file);
 			break;
 
 		case 2:
-			System.out.println("Enter the number of Continents:");
-			int numberOfContinents = Integer.parseInt(br.readLine());
-			HashMap<String, Integer> continentDetails = new HashMap<String, Integer>();
-			System.out.println("Enter the continent details in below format:");
-			System.out.println("Continent name = Control Value");
-			for (int i = 0; i < numberOfContinents; i++) {
-				String continent = br.readLine();
-				String[] split = continent.split("=");
-				continentDetails.put(split[0], Integer.parseInt(split[1]));
-			}
+			setContinentDetails(file);
 			break;
 
 		case 3:
-			System.out.println("Enter the continent to be removed from the map:");
-			String deleteContinent = br.readLine();
+			deleteContinentDetails(file);
+//			System.out.println("Enter the continent to be removed from the map:");
+//			String deleteContinent = br.readLine();
 			break;
 
 		case 4:
@@ -372,15 +376,318 @@ public class LoadAndEditMap {
 			break;
 		default:
 			System.out.println("Invalid option. Please choose the correct option.");
-			edition();
+			edition(file);
 
 		}
+		
 	}
-
-	public void changeMetadata() {
-
+	//ArrayList<String> mapTagData = new ArrayList<String>();
+	public void changeMetadata(String file) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Please Enter the image name of the map in below format:");
+		System.out.println("Imagename.bmp");
+		Pattern pattern = Pattern.compile("[a-z, A-Z]+.[bmp]+");
+		String name = br.readLine();
+		Matcher match = pattern.matcher(name.trim());	
+		while(!match.matches())
+		{
+			System.out.println("\n Please enter valid image name ");
+			name = br.readLine();
+			match = pattern.matcher(name.trim());
+		}
+		//mapGraph.getTagData().put("image", name.trim());
+		String image = "image=" + name.trim();
+		
+		System.out.println("Please specify yes or no for warn");
+		String warnanswer = br.readLine();
+		while(!((warnanswer.equals("yes"))||(warnanswer.equals("Yes")))) 
+		{
+			System.out.println("Please enter yes or no ");
+			warnanswer = br.readLine();
+		}
+		//mapGraph.getTagData().put("warn", warnanswer.trim());
+		String warn = "warn=" + warnanswer.trim();
+		
+		System.out.println("Please specify horizontal or vertical scroll");
+		String scrollanswer = br.readLine();
+		while(!((scrollanswer.equals("horizontal"))||(scrollanswer.equals("Horizontal"))))
+		{
+			System.out.println("Please enter valid response ");
+			scrollanswer = br.readLine();
+		}
+		//mapGraph.getTagData().put("scroll", scrollanswer.trim());
+		String scroll = "scroll=" + scrollanswer.trim();
+		
+		System.out.println("Please specify yes or no for wrap");
+		String wrapanswer = br.readLine();
+		while(!((wrapanswer.equals("no"))||(wrapanswer.equals("No"))))
+		{
+			System.out.println("Please enter yes or no ");
+			wrapanswer = br.readLine();
+		}
+		//mapGraph.getTagData().put("wrap", wrapanswer.trim());
+		String wrap = "wrap=" + wrapanswer.trim();
+		
+		System.out.println("Please enter the author name:");
+		//mapGraph.getTagData().put("author", br.readLine().trim());
+		String author = "author=" + br.readLine().trim();
+		String workingDir = System.getProperty("user.dir");
+		String fileName = file.trim();
+		// file path for windows : \\src\\com\\riskgame\\maps\\
+		// file path for mac: /src/com/riskgame/maps/
+		String filepath = workingDir + "/src/com/riskgame/maps/" + file;
+		File inputFile = new File(filepath);
+		File tempFile = new File(workingDir + "/src/com/riskgame/maps/myTempFile.map");
+		//BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile));
+		PrintWriter outputStream = new PrintWriter(tempFile);
+		//outputStream.println(mapTagData[0]);	//write the image file
+//		outputStream.println(mapTagData[1]);	//write the author
+//		outputStream.flush();
+//		outputStream.close();
+		FileReader filereader = new FileReader(filepath);
+		BufferedReader br_file = new BufferedReader(filereader);
+		//BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+		//BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile));
+		String currentLine= br_file.readLine();
+//		while(currentLine !=null) {
+//			writer.write(currentLine);
+			//for(String s:mapGraph.getTagData().keySet()) {
+				//outputStream.println(s+"="+mapGraph.getTagData().get(s));
+			//}
+//		}
+				outputStream.println("[Map]");
+				outputStream.println(image);
+				outputStream.println(warn);
+				outputStream.println(scroll);
+				outputStream.println(wrap);
+				outputStream.println(author);
+//		writer.write(image);
+//		writer.write(warn);
+//		writer.write(scroll);
+//		writer.write(wrap);
+//		writer.write(author);
+//				while (currentLine.trim().isEmpty()) {
+//					outputStream.println("\n");
+//					currentLine = br_file.readLine();
+//					
+//				}
+		Pattern tagData_pattern = Pattern.compile("[Map]+");
+		currentLine = currentLine.replaceAll("\\[", "").replaceAll("\\]", "");
+		Matcher tagData_match = tagData_pattern.matcher(currentLine.trim());
+		if(tagData_match.matches()) {
+			while(!currentLine.trim().isEmpty())
+				currentLine = br_file.readLine();
+		}
+		while (currentLine.trim().isEmpty()) {
+			outputStream.println();
+			currentLine = br_file.readLine();
+			
+		}
+		Pattern continents_pattern = Pattern.compile("[Continents]+");
+		currentLine = currentLine.replaceAll("\\[", "").replaceAll("\\]", "");
+		Matcher continent_match = continents_pattern.matcher(currentLine.trim());
+		
+		while(!continent_match.matches()) {
+			System.out.println(currentLine);
+			currentLine=br_file.readLine();
+			currentLine = currentLine.replaceAll("\\[", "").replaceAll("\\]", "");
+			continent_match = continents_pattern.matcher(currentLine.trim());	
+		}
+		
+		outputStream.println("[Continent]");
+		//writer.write("[Continent]");
+		while((currentLine = br_file.readLine()) != null) {
+			//String trimmedLine = currentLine.trim();
+			outputStream.println(currentLine);
+			//writer.write(currentLine );
+		}
+		outputStream.flush();
+		outputStream.close();
+		tempFile.renameTo(inputFile);
+		
+//		for(String s:mapGraph.getTagData().keySet())
+//		{
+//			writer.write(cbuf);
+//		}
+		//String author = "author=" + br.readLine().trim();
+			
+//		mapTagData.add(image);
+//		mapTagData.add(author);
+//		mapTagData.add(warn);		
+//		mapTagData.add(wrap);
+//		mapTagData.add(scroll);
+//		
+//		maptag.setMapTagData(mapTagData);		
+		System.out.println("\nMaptagdata added");
 	}
-	
+	public void setContinentDetails(String file) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Please enter continent details in below format:");
+		System.out.println("Continent name=Control Value");
+		Pattern pattern = Pattern.compile("[a-z, A-Z]+=[0-9]+");
+		String continentName = br.readLine();
+	    Matcher match = pattern.matcher(continentName.trim());
+	    //Continent continent=new Continent();
+	    while(!match.matches()) {
+	    	System.out.println("Pleae enter continent details in below format:");
+			System.out.println("Continent name=Control Value");
+			continentName = br.readLine();
+		    match = pattern.matcher(continentName.trim());
+//	    	String[] str=continentName.split("=");
+//	    	continent.getContinents().put(str[0],Integer.parseInt(str[1]));
+	    }
+	    if(match.matches()) {
+	    	String workingDir = System.getProperty("user.dir");
+			String fileName = file.trim();
+	    	String filepath = workingDir + "/src/com/riskgame/maps/" + file;
+			File inputFile = new File(filepath);
+			File tempFile = new File(workingDir + "/src/com/riskgame/maps/myTempFile.map");
+			PrintWriter outputStream = new PrintWriter(tempFile);
+			FileReader filereader = new FileReader(filepath);
+			BufferedReader br_file = new BufferedReader(filereader);
+			String currentLine= br_file.readLine();
+			String[] str=continentName.split("=");
+			Pattern tagData_pattern = Pattern.compile("[Map]+");
+			currentLine = currentLine.replaceAll("\\[", "").replaceAll("\\]", "");
+			Matcher tagData_match = tagData_pattern.matcher(currentLine.trim());
+			if(tagData_match.matches()) {
+				while(!currentLine.trim().isEmpty()) {
+					outputStream.println(currentLine);
+					currentLine = br_file.readLine();
+				}
+			}
+			while (currentLine.trim().isEmpty()) {
+				outputStream.println();
+				currentLine = br_file.readLine();
+				
+			}
+			Pattern continents_pattern = Pattern.compile("[Continents]+");
+			currentLine = currentLine.replaceAll("\\[", "").replaceAll("\\]", "");
+			Matcher continent_match = continents_pattern.matcher(currentLine.trim());
+			
+			while(!continent_match.matches()) {
+				System.out.println(currentLine);
+				currentLine=br_file.readLine();
+				currentLine = currentLine.replaceAll("\\[", "").replaceAll("\\]", "");
+				continent_match = continents_pattern.matcher(currentLine.trim());	
+			}
+			
+			outputStream.println("[Continent]");
+			currentLine=br_file.readLine();
+			if(mapGraph.getContinents().get(str[0]) != null) {
+				String[] cont=currentLine.trim().split("=");
+				while(!cont[0].equals(str[0])) {
+					outputStream.println(currentLine);
+					currentLine=br_file.readLine();
+					cont=currentLine.trim().split("=");
+					
+				}
+				if(cont[0].equals(str[0])) {
+					outputStream.println(cont[0]+"="+str[1]);
+					currentLine=br_file.readLine();
+				}
+			}
+			else {
+				Continent continent=new Continent();
+				continent.setName(str[0]);
+				continent.setControl_value(Integer.parseInt(str[1]));
+				mapGraph.getContinents().put(str[0], continent);
+				outputStream.println(continentName);
+				outputStream.println(currentLine);
+				currentLine = br_file.readLine();
+			}
+			while(currentLine != null) {
+				//String trimmedLine = currentLine.trim();
+				outputStream.println(currentLine);
+				currentLine = br_file.readLine();
+				//writer.write(currentLine );
+			}
+			outputStream.flush();
+			outputStream.close();
+			tempFile.renameTo(inputFile);
+	    }
+	}
+	public void deleteContinentDetails(String file) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Please enter the continent you want to delete:");
+		String continentName = br.readLine();
+		
+		if(mapGraph.getContinents().get(continentName.trim()) ==null) {
+			System.out.println("Continent doesn't exist");
+		}
+		else {
+			Continent continent=new Continent();
+			continent=mapGraph.getContinents().get(continentName.trim());
+			if((continent.getCountriesInContinent() != null) && (!continent.getCountriesInContinent().isEmpty())) {
+				//System.out.println("this is shit"+continent.getCountriesInContinent().get(0));
+				System.out.println("Cannot delete continent");
+			}
+			 else{
+			      continent.getContinents().remove(continentName.trim());
+			      mapGraph.getContinents().remove(continentName);
+			      System.out.println("The continent " + continentName.trim() + " is deleted from the map.");
+			      String workingDir = System.getProperty("user.dir");
+					String fileName = file.trim();
+			    	String filepath = workingDir + "/src/com/riskgame/maps/" + file;
+					File inputFile = new File(filepath);
+					File tempFile = new File(workingDir + "/src/com/riskgame/maps/myTempFile.map");
+					PrintWriter outputStream = new PrintWriter(tempFile);
+					FileReader filereader = new FileReader(filepath);
+					BufferedReader br_file = new BufferedReader(filereader);
+					String currentLine= br_file.readLine();
+					String[] str=continentName.split("=");
+					Pattern tagData_pattern = Pattern.compile("[Map]+");
+					currentLine = currentLine.replaceAll("\\[", "").replaceAll("\\]", "");
+					Matcher tagData_match = tagData_pattern.matcher(currentLine.trim());
+					if(tagData_match.matches()) {
+						while(!currentLine.trim().isEmpty()) {
+							outputStream.println(currentLine);
+							currentLine = br_file.readLine();
+						}
+					}
+					while (currentLine.trim().isEmpty()) {
+						outputStream.println();
+						currentLine = br_file.readLine();
+						
+					}
+					Pattern continents_pattern = Pattern.compile("[Continents]+");
+					currentLine = currentLine.replaceAll("\\[", "").replaceAll("\\]", "");
+					Matcher continent_match = continents_pattern.matcher(currentLine.trim());
+					
+					while(!continent_match.matches()) {
+						System.out.println(currentLine);
+						currentLine=br_file.readLine();
+						currentLine = currentLine.replaceAll("\\[", "").replaceAll("\\]", "");
+						continent_match = continents_pattern.matcher(currentLine.trim());	
+					}
+					
+					outputStream.println("[Continent]");
+					currentLine=br_file.readLine();
+					Pattern delete_pattern = Pattern.compile(continentName);
+					Matcher delete_match = continents_pattern.matcher(currentLine.trim());
+					String[] cont=currentLine.trim().split("=");
+					while(!cont[0].equals(continentName.trim())&&(currentLine !=null)) {
+						outputStream.println(currentLine);
+						currentLine=br_file.readLine();
+						cont=currentLine.trim().split("=");
+						
+					}
+					if(cont[0].equals(continentName.trim())) {
+						currentLine=br_file.readLine();
+					}
+					while(currentLine != null) {
+						//String trimmedLine = currentLine.trim();
+						outputStream.println(currentLine);
+						currentLine = br_file.readLine();
+						//writer.write(currentLine );
+					}
+					outputStream.flush();
+					outputStream.close();
+					tempFile.renameTo(inputFile);
+					
+			   }
+		}
+	}
 	public void DFS(HashMap<Country, ArrayList<Country>> adjacentCountries, Country country) {
 		
 		//System.out.println(country.getAdjacentCountries());
