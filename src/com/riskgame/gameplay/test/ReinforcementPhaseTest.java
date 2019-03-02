8c88a2c4b2f376ae2b10a82e13354f8b4b9b3c8d
Anusha KT <kt.anusha21@gmail.com>
2019-03-02 16:05:23 -0500

    Adding Testes for Startup phase

---
+++ src/com/riskgame/gameplay/test/ReinforcementPhaseTest.java
@@ -0,0 +1,54 @@
+package com.riskgame.gameplay.test;
+
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import com.riskgame.common.Continent;
+import com.riskgame.common.Country;
+import com.riskgame.common.RiskPlayer;
+import com.riskgame.gameplay.ReinforcementPhase;
+
+public class ReinforcementPhaseTest {
+    RiskPlayer player, player1;
+    Country country;
+    Continent continent;
+    
+    public ReinforcementPhaseTest() {
+        
+        continent = new Continent();
+        player = new RiskPlayer();
+        player1 = new RiskPlayer();
+        country = new Country();
+        country.setName("Egypt");
+        player.getMyCountries().add(country);
+        player1.getMyCountries().add(country);
+        continent.getCountriesListInContinent().add(country);
+        
+        country = new Country();
+        country.setName("Libya");
+        player.getMyCountries().add(country);
+        player1.getMyCountries().add(country);
+        continent.getCountriesListInContinent().add(country);
+        
+        country = new Country();
+        country.setName("Morocco");
+        player.getMyCountries().add(country);
+        continent.getCountriesListInContinent().add(country);
+        
+        
+        continent.setName("Northern Africa");
+        continent.setControlValue(4);
+    }
+    
+    @Test
+    public void isControlValueAssignedToPlayer() {
+        assertEquals(7, ReinforcementPhase.armiesToBeAssigned(player, continent));
+    }
+    
+    @Test
+    public void isControlValueNotAssignedToPlayer() {
+        assertNotEquals(continent.getControlValue(), ReinforcementPhase.armiesToBeAssigned(player1, continent));
+    }
+}
--- src/com/riskgame/gameplay/test/StartupPhaseTest.java
+++ src/com/riskgame/gameplay/test/StartupPhaseTest.java
@@ -2,14 +2,21 @@ package com.riskgame.gameplay.test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.HashMap;
+
 import org.junit.jupiter.api.Test;
 
+import com.riskgame.common.Country;
+import com.riskgame.common.GameMapGraph;
 import com.riskgame.common.RiskPlayer;
 import com.riskgame.gameplay.StartupPhase;
 
 public class StartupPhaseTest {
-    RiskPlayer player1, player2, player3;
+    RiskPlayer player1;
     StartupPhase startUpPhase;
+    Country country; 
+    GameMapGraph mapGraph = new GameMapGraph();
+    
     
     public StartupPhaseTest() {
         startUpPhase = new StartupPhase();
@@ -18,11 +25,24 @@ public class StartupPhaseTest {
         player1 = new RiskPlayer();
         startUpPhase.getPlayersList().add(player1);
         
+        
+        HashMap<String, Country> countrySet = new HashMap<String, Country>();
+        country = new Country();
+        country.setName("Morocco");
+        countrySet.put(country.getName(), country);
+        mapGraph.setCountrySet(countrySet);
+        
     }
     
     @Test
     public void testAllocationArmy() {
         startUpPhase.allocationOfArmyToPlayers();
         assertEquals(40, player1.getArmyCount());
+    }
+    
+    @Test
+    public void testallocationOfArmyToCountriesInitially() {
+    	startUpPhase.allocationOfArmyToCountriesInitially(mapGraph);
+    	assertEquals(1, country.getNoOfArmies() );
     }
 }
