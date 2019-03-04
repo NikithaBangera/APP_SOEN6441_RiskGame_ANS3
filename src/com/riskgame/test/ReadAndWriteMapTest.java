83d2a3c0fb28425da66fd9bdda126763f218d88e
Anusha KT <kt.anusha21@gmail.com>
2019-03-03 20:04:19 -0500

    Merging with shresthi branch and adding test cases

--- src/com/riskgame/test/ReadAndWriteMapTest.java
+++ src/com/riskgame/test/ReadAndWriteMapTest.java
@@ -30,8 +30,8 @@ public class ReadAndWriteMapTest {
     
     public ReadAndWriteMapTest() {
         readAndWriteMap = new ReadAndWriteMap();
-        validMap = "resources/maps/africa.map";
-        invalidMap = "resources/maps/Asia.map";
+        validMap = "resources\\maps\\test.map";
+        invalidMap = "resources\\maps\\Asia.map";
         validCountryData = "[Country] Libya,314,94,Northern Africa,Tunisia,Algeria,Egypt";
         invalidCountryData = "[Country]\n,133,266,Northern Ireland,Ireland";
         validContinentData = "[Continent]\nSouth America=2";
