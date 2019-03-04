f1d4bbc7a0e985ddc504c64cc4035708d1e8eaaa
skasul <sumeetha.k@gmail.com>
2019-03-03 19:14:45 -0500

    Added Javadoc to Continent

--- src/com/riskgame/common/Continent.java
+++ src/com/riskgame/common/Continent.java
@@ -1,65 +1,78 @@
-package com.riskgame.common;
+package com.riskgame.model;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class Continent {
 
+public class Continent {
+	
+	/** Variable to store Continent name */
 	String continentName;
+	
+	/** Stores Control value */
 	int controlValue;
+	
+	/** HashMap to store Continent name with associated control value */
 	HashMap<String, Integer> continentDetails = new HashMap<String, Integer>();
+	
+	/** ArrayList to store list of Countries name associated with a Continent*/
 	ArrayList<Country> countriesInContinent = new ArrayList<Country>();
 
+	
 	public String getContinentName() {
 		return continentName;
 	}
 
+	/**
+	 * Sets the name of the continent 
+	 *
+	 * @param Continent name
+	 */
 	public void setName(String name) {
 		this.continentName = name;
 	}
 
+	/**
+	 * Gets the control value 
+	 *
+	 * @return Country value
+	 */
 	public int getControlValue() {
 		return controlValue;
 	}
 
-	public void setControl_value(int controlValue) {
+	public void setControlValue(int controlValue) {
 		this.controlValue = controlValue;
 	}
 
+	
 	public HashMap<String, Integer> getContinents() {
 		return continentDetails;
 	}
 
+	
 	public void setContinents(HashMap<String, Integer> continents) {
 		this.continentDetails = continents;
 	}
 
-	public ArrayList<Country> getCountriesListInContinent() {
+	
+	public ArrayList<Country> getCountriesInContinent() {
 		return countriesInContinent;
 	}
 
 	/**
-	 * Method to add a country to the list of countries in the continent
-	 * 
-	 * @param country country to be added
+	 * Sets the Countries list associated with a Continent 
+	 *
+	 * @param list of Countries
 	 */
-	public void addCountry(Country country) {
-		countriesInContinent.add(country);
-	}
-
-	/**
-	 * Set the list of countries.
-	 * 
-	 * @param listOfCountries list of countries to set
-	 */
-	public void setCountriesListInContinent(ArrayList<Country> countriesInContinent) {
+	public void setCountriesInContinent(ArrayList<Country> countriesInContinent) {
 		this.countriesInContinent = countriesInContinent;
 	}
 
 	@Override
 	public String toString() {
-		return "Continent [continentName=" + continentName + ", controlValue=" + controlValue + ", continentDetails="
-				+ continentDetails + ", countriesInContinent=" + countriesInContinent + "]";
+		return "Continents: \n[continentName=" + continentName + ", controlValue=" + controlValue + ", continentDetails="
+				+ continentDetails + ", countriesInContinent=" + countriesInContinent + "]/n";
 	}
 
 }
