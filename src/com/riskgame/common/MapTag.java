62e3274f642397a93b69df6a9e47489fba9fc142
skasul <sumeetha.k@gmail.com>
2019-03-03 19:56:27 -0500

    added javadoc and updated model structure

--- src/com/riskgame/common/MapTag.java
+++ src/com/riskgame/common/MapTag.java
@@ -1,23 +1,127 @@
-package com.riskgame.common;
+package com.riskgame.model;
 
-import java.util.ArrayList;
-
+/**
+ * This class stores all the Map tag data.It stores Author name, Image name,
+ * Scroll, Warn and Wrap options for the Map.
+ * 
+ * @author 
+ * @author 
+ *
+ */
 public class MapTag {
 
-	public ArrayList<String> mapTagData = new ArrayList<String>();
+	/**Name of the author for the map.*/
+	private String authorName;
+	
+	/** Warn option for the map. */
+	private String warn;
+	
+	/** Stores the name of the Image. */
+	private String imageName;
+	
+	/** Wrap option for the map.*/
+	private String wrap;
+	
+	/** Scroll option for the map.*/
+	private String scroll;
 
-	public ArrayList<String> getMapTagData() {
-		return mapTagData;
+	
+	public MapTag() {
+		super();
 	}
 
-	public void setMapTagData(ArrayList<String> mapTagData) {
-		this.mapTagData = mapTagData;
+	
+	public MapTag(String authorName, String warn, String imageName, String wrap, String scroll) {
+		super();
+		this.authorName = authorName;
+		this.warn = warn;
+		this.imageName = imageName;
+		this.wrap = wrap;
+		this.scroll = scroll;
+	}
+
+	/**
+	 * Gets name of an Author 
+	 *
+	 * @return Author name
+	 */
+	public String getAuthorName() {
+		return authorName;
+	}
+	
+	/**
+	 * Sets Author name for the map 
+	 *
+	 * @param Author name
+	 */
+	public void setAuthorName(String authorName) {
+		this.authorName = authorName;
+	}
+
+	/**
+	 * Gets Warn option for the map 
+	 *
+	 * @return Warn option as string 
+	 */
+	public String getWarn() {
+		return warn;
+	}
+
+	/**
+	 * Sets Warn option for the map 
+	 *
+	 * @param Warn option
+	 */
+	public void setWarn(String warn) {
+		this.warn = warn;
+	}
+
+	/**
+	 * Gets name of the Image 
+	 *
+	 * @return Image name as String
+	 */
+	public String getImageName() {
+		return imageName;
+	}
+
+	/**
+	 * Sets name of the Image 
+	 *
+	 * @param Image name
+	 */
+	public void setImageName(String imageName) {
+		this.imageName = imageName;
+	}
+
+	
+	public String getWrap() {
+		return wrap;
+	}
+
+	
+	public void setWrap(String wrap) {
+		this.wrap = wrap;
+	}
+
+	/**
+	 * Gets Scroll option for the map 
+	 *
+	 * @return Scroll option as string
+	 */
+	public String getScroll() {
+		return scroll;
+	}
+
+	
+	public void setScroll(String scroll) {
+		this.scroll = scroll;
 	}
 
 	@Override
 	public String toString() {
-		return "MapTag [mapTagData=" + mapTagData + "]";
+		return "[Map]\nAuthor Name = " + authorName + "\nWarn = " + warn + "\nImage Name = " + imageName + "\nWrap = "
+				+ wrap + "\nScroll = " + scroll + "\n";
 	}
-	
-	
+
 }
