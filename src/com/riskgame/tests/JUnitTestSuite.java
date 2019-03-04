058a9b49d6f9472e027be40c26e52ccd4cf035da
skasul <sumeetha.k@gmail.com>
2019-03-04 14:44:14 -0500

    added suite

---
+++ src/com/riskgame/tests/JUnitTestSuite.java
@@ -0,0 +1,18 @@
+package com.riskgame.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({CreateAndEditMapTest.class,ReadAndWriteMapTest.class,StartupPhaseTest.class,
+    FortificationPhaseTest.class,ReinforcementPhaseTest.class})
+
+/**
+ * A TestSuite class for testing all the test cases
+ * @author 
+ *
+ */
+public class JUnitTestSuite {
+
+}
